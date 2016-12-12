f8bc85a91007f4bbbe19436dd1f2e550f00825ca
zheng <michael@acnet.co>
2016-12-12 20:08:00 +0800

    update：执行了executePendingTransactions之后isAdded才会生效

--- app/src/main/java/allconnected/co/fragmenttest/MainActivity.java
+++ app/src/main/java/allconnected/co/fragmenttest/MainActivity.java
@@ -20,6 +20,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private FragmentOne mFOne;
+    private FragmentManager mFm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,36 +38,66 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
 
-        FragmentManager fm = getSupportFragmentManager();
+        mFm = getSupportFragmentManager();
 //       通过日志查看savedinstancestate里面保存的内容
         Log.d("FragmentOne", "onCreate: " + savedInstanceState);
 //       每一个fragment的操作都启用一个事务，保证事务的原子性
-        if (savedInstanceState == null) {
-            mFOne = new FragmentOne();
-            FragmentTransaction tx;
-            tx = fm.beginTransaction();
-            tx.add(R.id.content_main, mFOne, "ONE");
-            tx.commit();
-            fm.executePendingTransactions();
-            if (mFOne.isAdded()) {
-                tx = fm.beginTransaction();
-                tx.remove(mFOne);
-                tx.commit();
-                fm.executePendingTransactions();
-            }
-            tx = fm.beginTransaction();
-            tx.add(R.id.content_main, mFOne, "ONE");
-            tx.commit();
-            fm.executePendingTransactions();//确保fragment是立刻提交的
+//        if (savedInstanceState == null) {
+        mFOne=new FragmentOne();
+        mFm.beginTransaction().add(R.id.content_main,mFOne,"mOne").commit();
+        mFm.executePendingTransactions();
+//        执行了executePendingTransactions，isAdded才会生效
+        if (!mFOne.isAdded()){
+            mFm.beginTransaction().add(R.id.content_main,mFOne,"mOne").commit();
         }
-        List<Fragment> fragments = fm.getFragments();
+//        tx = fm.beginTransaction();
+//        tx.add(R.id.content_main, mFOne, "ONE");
+//        tx.attach(mFOne);
+//        tx.attach(mFOne);
+//        tx.attach(mFOne);
+//        tx.commit();
+//        fm.executePendingTransactions();
+
+
+        List<Fragment> fragments = mFm.getFragments();
 //        如果想屏幕翻转的时候fragments列表为空则注释掉这行，不进行保存
 //        super.onSaveInstanceState(outState);
 //        或者在add fragment的时候判断savedInstanceState为空的时候再进行add操作
-        if (fragments != null && fragments.size() > 0) {
+        if (fragments != null && fragments.size() > 0)
+
+        {
             Toast.makeText(this, "we have " + fragments.size() + " fragmentOne(s) ", Toast.LENGTH_SHORT).show();
         }
 
+        FloatingActionButton floatingActionButton = (FloatingActionButton) findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction tx;
+                if (mFOne == null) {
+                    mFOne = new FragmentOne();
+                    tx = mFm.beginTransaction();
+                    tx.add(R.id.content_main, mFOne, "ONE");
+                    tx.commit();
+                    mFm.executePendingTransactions();
+                } else {
+                    if (mFOne.isAdded()) {
+                        tx = mFm.beginTransaction();
+                        tx.show(mFOne).commit();
+                    } else {
+                        tx = mFm.beginTransaction();
+                        tx.remove(mFOne).commit();
+//
+                        mFOne = new FragmentOne();
+                        tx = mFm.beginTransaction();
+                        tx.add(R.id.content_main, mFOne, "ONE");
+                        tx.commit();
+                        mFm.executePendingTransactions();
+                    }
+                }
+
+            }
+        });
 
     }
 
@@ -103,6 +134,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         int id = view.getId();
         switch (id) {
             case R.id.btn_one:
+                mFOne = null;
                 Toast.makeText(this, "this is from fragment", Toast.LENGTH_SHORT).show();
                 break;
         }
